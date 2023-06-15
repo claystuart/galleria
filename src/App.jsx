45a4ea26348d6c519881217e959a7720f8d78716
Clay Stuart <clay.stuart@gmail.com>
2023-06-15 15:37:20 -0400

    Add frontEnd Mentor favicon to index.html.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      Start slideshow
+
+      Starry Night
+      Vincent Van Gogh
+
+      Girl with a Pearl Earring
+      Johannes Vermeer
+
+      Guernica
+      Pablo Picasso
+
+      Penitent Magdalene
+      Artemisia Gentileschi
+
+      The Storm on the Sea of Galilee
+      Rembrandt
+
+      The Great Wave off Kanagawa
+      Hokusai
+
+      Van Gogh Self-portrait
+      Vincent Van Gogh
+
+      The Sleeping Gypsy
+      Henri Rousseau
+
+      Lady with an Ermine
+      Leonardo da Vinci
+
+      The Night Café
+      Vincent Van Gogh
+
+      The Basket of Apples
+      Paul Cézanne
+
+      The Boy in the Red Vest
+      Paul Cézanne
+
+      Arnolfini Portrait
+      Jan van Eyck
+
+      Mona Lisa
+      Leonardo da Vinci
+
+      The Swing
+      Jean-Honoré Fragonard
     </>
   )
 }
